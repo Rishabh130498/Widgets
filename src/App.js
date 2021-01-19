e01025f64cf79c3c8eef91daf5e4616cb89dbf98
Rishabh Agarwal <Rishabh130498@gmail.com>
2021-01-20 01:24:49 +0530

    Accordion.js is created and assigned an array of objects called items which is passed from app to Accordion as a props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React ?",
+    content: "React is javascript frontend Library",
+  },
+  {
+    title: "Why use React ?",
+    content: "React is favourite JS library among Engineers",
+  },
+  {
+    title: "How do you use React ?",
+    content: "You can use React by creating components",
+  },
+];
 
 export default () => {
-  return <h1>Widget App</h1>;
+  return (
+    <h1>
+      <Accordion items={items} />
+    </h1>
+  );
 };
