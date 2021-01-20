36e02c6b0afd7845f8b5468e930357ac06b09353
Rishabh Agarwal <Rishabh130498@gmail.com>
2021-01-20 18:18:02 +0530

    added expanding and contracting feature using classname 'active' keyword

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -8,24 +8,24 @@ const Accordion = ({ items }) => {
   };
 
   const RenderedItems = items.map((item, index) => {
+    const Active = index === ActiveIndex ? "active" : "";
+
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClicked(index)}>
+        <div
+          className={`title ${Active}`}
+          onClick={() => onTitleClicked(index)}
+        >
           <i className="dropdown icon" />
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${Active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return (
-    <div className="ui styled accordion">
-      {RenderedItems}
-      {ActiveIndex}
-    </div>
-  );
+  return <div className="ui styled accordion">{RenderedItems}</div>;
 };
 
 export default Accordion;
