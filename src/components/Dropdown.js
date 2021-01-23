969854ceb8c96ba3068f483470e20777b6e08576
Rishabh Agarwal <Rishabh130498@gmail.com>
2021-01-24 03:24:38 +0530

    Filtering the option list has been done and the selected item now not showing in the option list

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,6 +2,10 @@ import React from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const renderedOptions = options.map((option) => {
+    if (selected.value === option.value) {
+      return null;
+    }
+
     return (
       <div
         onClick={() => onSelectedChange(option)}
